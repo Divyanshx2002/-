d2d83c199eea54782a9c1a40152fefcca1d66e22
Divyanshx2002 <divyansh235368@gmail.com>
2023-01-02 15:43:34 +0530

    2nd commit

--- script.js
+++ script.js
@@ -3,31 +3,61 @@ let decrease = document.querySelector('#dec1');
 let reset = document.querySelector('#reset');
 let increase = document.querySelector('#inc1');
 let head = document.querySelector('#h1');
-
-
-function increamentNum(num) {
-    numHistory = ++num;
-    return num;
-}
-
-function decrementNum(num) {
-    numHistory = --num;
-    return num;
-}
-
+let incrementIntervalID;
+let decrementIntervalID;
+let resetIntervalID;
 
 increase.addEventListener('click', function () {
-    setInterval(() => {
-        let incrementNum = increamentNum(numHistory);
-        head.innerHTML = incrementNum;
-    }, 1000);
+    if (decrementIntervalID) {
+        clearInterval(decrementIntervalID)
+    }
+    if (!incrementIntervalID) {
+        incrementIntervalID = setInterval(() => {
+            let increaseNum = increamentNum(numHistory);
+            head.innerHTML = increaseNum;
+        }, 1000);
+    }
 })
 
 decrease.addEventListener('click', function () {
-    setInterval(() => {
-        let decrementNum = decrementNum(numHistory);
-        head.innerHTML = decrementNum;
-    }, 1000);
+    if (incrementIntervalID) {
+        clearInterval(incrementIntervalID)
+    }
+    if (!decrementIntervalID) {
+        decrementIntervalID = setInterval(() => {
+            let decreaseNum = decrementNum(numHistory);
+            head.innerHTML = decreaseNum;
+        }, 1000);
+    }
 });
 
+reset.addEventListener('click', function(){
+    if (decrementIntervalID) {
+        clearInterval(decrementIntervalID)
+    }
+    if (incrementIntervalID) {
+        clearInterval(incrementIntervalID)
+    }
+    if (!resetIntervalID){
+        let reset = resetNum(numHistory);
+        head.innerHTML = 0;
+    }
+});
 
+function increamentNum(num) {
+    num++;
+    numHistory = num;
+    return numHistory;
+}
+
+function decrementNum(num) {
+    num--;
+    numHistory = num;
+    return numHistory;
+}
+
+function resetNum(num) {
+    num = 0;
+    numHistory = num;
+    return numHistory;
+}
